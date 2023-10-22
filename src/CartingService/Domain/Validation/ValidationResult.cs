4eeeeabef993d5178cabc8fd49d76a12de7efc54
KobernykYura <yury.kabernik.berazouski@gmail.com>
2023-10-22 20:37:47 +0400

    tests(Domain) validation result factory

--- src/CartingService/Domain/Validation/ValidationResult.cs
+++ src/CartingService/Domain/Validation/ValidationResult.cs
@@ -2,6 +2,9 @@
 
 public struct ValidationResult
 {
+    public static readonly ValidationResult Success = new(true, null);
+    public static ValidationResult FailedFrom(string propertyName) => new(false, propertyName);
+
     internal ValidationResult(bool isValid, string? name)
     {
         this.IsValid = isValid;
@@ -10,7 +13,4 @@ public struct ValidationResult
 
     public bool IsValid { get; }
     public string? PropertyName { get; }
-
-    public static readonly ValidationResult Success = new(true, null);
-    public static ValidationResult FailedFrom(string propertyName) => new(false, propertyName);
 }
