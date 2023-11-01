c109f547ad409176ff2902cf90a199bb1227fc93
KobernykYura <yury.kabernik.berazouski@gmail.com>
2023-11-01 12:22:24 +0400

    feat: DAL type mapper Uri

---
+++ src/CatalogService/DataAccess/Mappers/SqlMapperRegistry.cs
@@ -0,0 +1,12 @@
+﻿using CatalogService.DataAccess.SqlMappers;
+using Dapper;
+
+namespace CatalogService.DataAccess;
+
+public class SqlMapperRegistry
+{
+    public static void Register()
+    {
+        SqlMapper.AddTypeHandler(new UriTypeMapper());
+    }
+}
---
+++ src/CatalogService/DataAccess/Mappers/UriTypeMapper.cs
@@ -0,0 +1,24 @@
+﻿using System.Data;
+using Dapper;
+
+namespace CatalogService.DataAccess.SqlMappers;
+
+public class UriTypeMapper : SqlMapper.TypeHandler<Uri>
+{
+    public override Uri? Parse(object value)
+    {
+        Uri? resultUri = default;
+
+        if (value is string strValue)
+        {
+            Uri.TryCreate(strValue, UriKind.RelativeOrAbsolute, out resultUri);
+        }
+
+        return resultUri;
+    }
+
+    public override void SetValue(IDbDataParameter parameter, Uri? value)
+    {
+        parameter.Value = value?.ToString();
+    }
+}
