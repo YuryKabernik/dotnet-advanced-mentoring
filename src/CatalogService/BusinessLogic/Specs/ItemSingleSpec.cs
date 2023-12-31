cc76341c532e37053975a9d1fb9821c525cac893
yuryk <yury.kabernik.berazouski@gmail.com>
2023-12-31 18:34:42 +0400

    fix(ItemSingleSpec) where by Id

--- src/CatalogService/BusinessLogic/Specs/ItemSingleSpec.cs
+++ src/CatalogService/BusinessLogic/Specs/ItemSingleSpec.cs
@@ -5,9 +5,10 @@ namespace CatalogService.BusinessLogic.Specs;
 
 public sealed class ItemSingleSpec : SingleResultSpecification<Item>
 {
-    public ItemSingleSpec()
+    public ItemSingleSpec(Guid id)
     {
         this.Query
+            .Where(i => i.Id == id)
             .Include(i => i.Category)
             .OrderBy(i => i.Name);
     }
