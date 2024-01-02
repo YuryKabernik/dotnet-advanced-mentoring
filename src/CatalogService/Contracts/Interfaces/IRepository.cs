a7ecb7f19b9c406a7485f05f4e30d5fae368ce63
yuryk <yury.kabernik.berazouski@gmail.com>
2024-01-02 21:55:38 +0400

    refactor(IRepository) custom interface implementation

--- src/CatalogService/Contracts/Interfaces/IRepository.cs
+++ src/CatalogService/Contracts/Interfaces/IRepository.cs
@@ -3,7 +3,11 @@ using CatalogService.Domain.Contracts.Entities;
 
 namespace CatalogService.Domain.Contracts.Interfaces;
 
-public interface IRepository<TSource> : IRepositoryBase<TSource>
+public interface IRepository<TSource>
     where TSource : Entity<Guid>
 {
+    Task<List<TSource>> ListAsync(ISpecification<TSource> spec, CancellationToken cancellationToken = default);
+    Task<TSource> AddAsync(TSource entity, CancellationToken cancellationToken = default);
+    Task DeleteAsync(TSource entity, CancellationToken cancellationToken = default);
+    Task UpdateAsync(TSource entity, CancellationToken cancellationToken = default);
 }
--- src/CatalogService/DataAccess/Repositories/Repository.cs
+++ src/CatalogService/DataAccess/Repositories/Repository.cs
@@ -1,14 +1,51 @@
-﻿using Ardalis.Specification.EntityFrameworkCore;
+﻿using Ardalis.Specification;
+using Ardalis.Specification.EntityFrameworkCore;
 using CatalogService.Domain.Contracts.Entities;
 using CatalogService.Domain.Contracts.Interfaces;
 using Microsoft.EntityFrameworkCore;
 
 namespace CatalogService.DataAccess.Repositories;
 
-public class Repository<TSource> : RepositoryBase<TSource>, IRepository<TSource>
+public class Repository<TSource> : IRepository<TSource>
     where TSource : Entity<Guid>
 {
-    public Repository(DbContext context) : base(context)
+    private readonly DbContext _dbContext;
+    private readonly ISpecificationEvaluator _specificationEvaluator;
+
+    public Repository(DbContext context)
     {
+        this._dbContext = context;
+        this._specificationEvaluator = SpecificationEvaluator.Default;
+    }
+
+    public Task<List<TSource>> ListAsync(ISpecification<TSource> spec, CancellationToken cancellationToken = default)
+    {
+        var source = this._dbContext.Set<TSource>().AsQueryable();
+        var query = this._specificationEvaluator.GetQuery(source, spec);
+
+        return query.ToListAsync(cancellationToken);
+    }
+
+    public async Task<TSource> AddAsync(TSource entity, CancellationToken cancellationToken = default)
+    {
+        _dbContext.Set<TSource>().Add(entity);
+
+        await _dbContext.SaveChangesAsync(cancellationToken);
+
+        return entity;
+    }
+
+    public async Task DeleteAsync(TSource entity, CancellationToken cancellationToken = default)
+    {
+        _dbContext.Set<TSource>().Remove(entity);
+
+        await _dbContext.SaveChangesAsync(cancellationToken);
+    }
+
+    public async Task UpdateAsync(TSource entity, CancellationToken cancellationToken = default)
+    {
+        _dbContext.Set<TSource>().Update(entity);
+
+        await _dbContext.SaveChangesAsync(cancellationToken);
     }
 }
