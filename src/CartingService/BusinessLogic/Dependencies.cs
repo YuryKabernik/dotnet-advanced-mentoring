d397748848b2f2bce1ba642d5db5e134ade53443
KobernykYura <yury.kabernik.berazouski@gmail.com>
2024-01-08 12:08:46 +0400

    fix(BusinessLogic) call DAL dependencies registrar

--- src/CartingService/BusinessLogic/Dependencies.cs
+++ src/CartingService/BusinessLogic/Dependencies.cs
@@ -1,11 +1,15 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using DataAccessDependencies = CartingService.DataAccess.Dependencies;
 
 namespace CartingService.BusinessLogic
 {
     public class Dependencies
     {
-        public static IServiceCollection Register(IServiceCollection services)
+        public static IServiceCollection Register(IServiceCollection services, IConfigurationRoot configuration)
         {
+            DataAccessDependencies.Register(services, configuration);
+
             services.AddMediatR(configuration => configuration.RegisterServicesFromAssemblyContaining<Dependencies>());
 
             return services;
