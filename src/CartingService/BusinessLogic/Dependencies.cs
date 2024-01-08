40c79381b94bdf40d522b6b0448370da6a763e76
KobernykYura <yury.kabernik.berazouski@gmail.com>
2024-01-08 11:58:02 +0400

    feat(BusinessLogic) define Dependencies registrar

---
+++ src/CartingService/BusinessLogic/Dependencies.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace CartingService.BusinessLogic
+{
+    public class Dependencies
+    {
+        public static IServiceCollection Register(IServiceCollection services)
+        {
+            services.AddMediatR(configuration => configuration.RegisterServicesFromAssemblyContaining<Dependencies>());
+
+            return services;
+        }
+    }
+}
