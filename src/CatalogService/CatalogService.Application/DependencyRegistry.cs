dca5e0208e5b1f1933a821714e6df416412351d8
KobernykYura <yury.kabernik.berazouski@gmail.com>
2024-01-09 18:32:32 +0400

    feat(Application) dependency registry

---
+++ src/CatalogService/CatalogService.Application/DependencyRegistry.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace Application
+{
+    public class DependencyRegistry
+    {
+        public static IServiceCollection AddDependencies(IServiceCollection services)
+        {
+            services.AddMediatR(configuration => configuration.RegisterServicesFromAssemblyContaining<DependencyRegistry>());
+
+            return services;
+        }
+    }
+}
